33b10b7c52fa79eba5cc5162ff888a5c09423289
Wojciech Jedynak <wjedynak@gmail.com>
2024-12-11 13:05:36 +0900

    add

--- src/my_robot_tutorials/src/publisher.cpp
+++ src/my_robot_tutorials/src/publisher.cpp
@@ -1,8 +1,9 @@
 #include <ros/ros.h>
 #include <std_msgs/String.h>
 
-int main(int argc, char **argv) {
-    ros::init(argc, argv, "publisher");
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "publisher", ros::init_options::AnonymousNode);
     ros::NodeHandle nh;
 
     ros::Publisher pub = nh.advertise<std_msgs::String>("/robot_news_radio", 10);
@@ -11,7 +12,8 @@ int main(int argc, char **argv) {
 
     ros::Rate rate(10);
 
-    while (ros::ok()) {
+    while (ros::ok())
+    {
         ROS_INFO("Hello");
         std_msgs::String msg;
         msg.data = "HI!!!";
