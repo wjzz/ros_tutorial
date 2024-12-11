f655d7cef63b4b674e39f8aabc3e1c22548415f3
Wojciech Jedynak <wjedynak@gmail.com>
2024-12-11 16:08:58 +0900

    fix name

--- src/my_robot_tutorials/scripts/04_service_client.py
+++ src/my_robot_tutorials/scripts/04_service_client.py
@@ -4,7 +4,7 @@ import rospy
 from rospy_tutorials.srv import AddTwoInts
 
 if __name__ == "__main__":
-    rospy.init_node("add two ints client")
+    rospy.init_node("add_two_ints_client")
 
     rospy.wait_for_service("/add_two_ints")
 
