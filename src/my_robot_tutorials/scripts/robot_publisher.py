1b8f8f7ab895e9a001a3d37a491113cd512d09b5
Wojciech Jedynak <wjedynak@gmail.com>
2024-12-11 10:38:28 +0900

    add

--- src/my_robot_tutorials/scripts/robot_publisher.py
+++ src/my_robot_tutorials/scripts/robot_publisher.py
@@ -3,14 +3,35 @@
 import rospy
 from std_msgs.msg import String
 
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Robot:
+    id: int
+    velocity: int
+
+
+def random_robot() -> Robot:
+    import random
+
+    ids = [1, 2, 3]
+
+    id = random.choice(ids)
+    velocity = random.randint(-10, 10)
+
+    return Robot(id, velocity)
+
 
 class Publisher:
     def __init__(self):
         self.pub = rospy.Publisher("/robot_news", String, queue_size=10)
 
-    def publish_message(self):
+    def publish_message(self, robot: Robot):
+        import json
+
         msg = String()
-        msg.data = "Hello"
+        msg.data = json.dumps(robot.__dict__)
         self.pub.publish(msg)
 
 
@@ -24,6 +45,8 @@ if __name__ == "__main__":
     while not rospy.is_shutdown():
         rospy.loginfo("Preparing a message")
         rate.sleep()
-        publisher.publish_message()
+
+        robot = random_robot()
+        publisher.publish_message(robot)
 
     rospy.loginfo("Robot publisher stopped")
