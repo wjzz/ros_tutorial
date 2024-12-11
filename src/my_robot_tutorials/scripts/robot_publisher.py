669e6ae1bad2278bb84103b9dbedab6908d688d9
Wojciech Jedynak <wjedynak@gmail.com>
2024-12-11 10:28:58 +0900

    add

--- src/my_robot_tutorials/scripts/robot_publisher.py
+++ src/my_robot_tutorials/scripts/robot_publisher.py
@@ -3,9 +3,27 @@
 import rospy
 from std_msgs.msg import String
 
+
+class Publisher:
+    def __init__(self):
+        self.pub = rospy.Publisher("/robot_news", String, queue_size=10)
+
+    def publish_message(self):
+        msg = String()
+        msg.data = "Hello"
+        self.pub.publish(msg)
+
+
 if __name__ == "__main__":
     rospy.init_node("my_robot_publisher")
     rospy.loginfo("Robot publisher started")
 
+    rate = rospy.Rate(10)
+    publisher = Publisher()
+
     while not rospy.is_shutdown():
         rospy.loginfo("Preparing a message")
+        rate.sleep()
+        publisher.publish_message()
+
+    rospy.loginfo("Robot publisher stopped")
