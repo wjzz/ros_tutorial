4e1e14c6526f0a60828ceb285e5b4b6be73bd80e
Wojciech Jedynak <wjedynak@gmail.com>
2024-12-12 12:59:35 +0900

    new msg type publisher

---
+++ src/my_robot_tutorials/scripts/07_hw_status_publisher.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import rospy
+from my_robot_msgs import HardwareStatus
+
+if __name__ == "__main__":
+    rospy.init_node("hardware publisher")
+    rospy.loginfo("This node has been started")
+
+    rate = rospy.Rate(10)  # 10 Hz
+
+    pub = rospy.Publisher("/hardware_status", HardwareStatus, queue_size=10)
+
+    while not rospy.is_shutdown():
+        rospy.loginfo("Publishing message")
+        rate.sleep()
+
+        msg = HardwareStatus()
+        msg.temperature = 45
+        msg.are_motors_up = True
+        msg.debug_message = "Everything OK"
+        pub.publish(msg)
+
+    rospy.loginfo("Node was stopped")
