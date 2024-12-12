142cc3f5f9b08ec06cb3e5f73274ee201f41fcbd
Wojciech Jedynak <wjedynak@gmail.com>
2024-12-12 09:32:46 +0900

    service client

---
+++ src/my_robot_tutorials/src/05_my_first_service_client.cpp
@@ -0,0 +1,27 @@
+#include <ros/ros.h>
+#include <rospy_tutorials/AddTwoInts.h>
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "add_two_inits_client");
+    ros::NodeHandle nh;
+    ROS_INFO("Client initialized");
+
+    ros::ServiceClient client = nh.serviceClient<rospy_tutorials::AddTwoInts>("/add_two_ints");
+
+    rospy_tutorials::AddTwoInts srv;
+    srv.a = 123;
+    srv.b = 100;
+
+    ROS_INFO("Sending request");
+    if (client.call(srv))
+    {
+        // process data
+        ROS_INFO("Returned sum is %d", (int)srv.response.sum);
+    }
+    else
+    {
+        // service failed
+        ROS_WARN("Service call failed");
+    }
+}
