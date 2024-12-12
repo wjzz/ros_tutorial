c187810d9fa497ca47d4e2b9fc82d340eb1735a0
Wojciech Jedynak <wjedynak@gmail.com>
2024-12-12 14:13:00 +0900

    cpp file

---
+++ src/my_robot_tutorials/src/06_params.cpp
@@ -0,0 +1,28 @@
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "publisher", ros::init_options::AnonymousName);
+    ros::NodeHandle nh;
+
+    ros::Publisher pub = nh.advertise<std_msgs::String>("/robot_news_radio", 10);
+
+    ROS_INFO("Node has been started");
+
+    // This is new
+    double publish_frequency;
+    nh.getParam("/number_publish_frequency", publish_frequency);
+    ros::Rate rate(publish_frequency;
+
+    nh.setParam("/another_param", 10);
+
+    while (ros::ok())
+    {
+        ROS_INFO("Hello");
+        std_msgs::String msg;
+        msg.data = "HI!!!";
+        pub.publish(msg);
+        rate.sleep();
+    }
+}
