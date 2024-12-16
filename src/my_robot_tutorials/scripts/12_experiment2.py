b81b3671554f6b7290e86a5437e3ed163c629b01
Wojciech Jedynak <wjedynak@gmail.com>
2024-12-16 10:55:07 +0900

    fix

--- src/my_robot_tutorials/scripts/12_experiment2.py
+++ src/my_robot_tutorials/scripts/12_experiment2.py
@@ -4,6 +4,8 @@ import rospy
 from std_msgs.msg import String
 
 import threading
+import time
+import os
 
 
 def callback_receive_data(msg):
@@ -16,6 +18,8 @@ if __name__ == "__main__":
     rospy.init_node("robot_subscriber")
     rospy.loginfo("Robot Subscriber started")
 
+    print(os.getpid())
+
     rate = rospy.Rate(10)  # 10 Hz
 
     pub = rospy.Subscriber("/robot_news", String, callback_receive_data)
@@ -24,3 +28,4 @@ if __name__ == "__main__":
         rospy.loginfo("=== Printing ===")
         for thread in threading.enumerate():
             rospy.loginfo(thread.name)
+        time.sleep(1)
