f36738f4635274c22e4472794678a2a47787d7e3
Wojciech Jedynak <wjedynak@gmail.com>
2024-12-16 10:53:34 +0900

    try

--- src/my_robot_tutorials/scripts/12_experiment2.py
+++ src/my_robot_tutorials/scripts/12_experiment2.py
@@ -7,14 +7,6 @@ import threading
 
 
 def callback_receive_data(msg):
-    rospy.loginfo("1: Message received")
-    rospy.loginfo(msg)
-    rospy.loginfo(f"handler= {threading.get_ident()}")
-
-
-def callback_receive_data2(msg):
-    rospy.loginfo("2: Message received")
-    rospy.loginfo(msg)
     rospy.loginfo(f"handler= {threading.get_ident()}")
     while True:
         pass
@@ -27,8 +19,8 @@ if __name__ == "__main__":
     rate = rospy.Rate(10)  # 10 Hz
 
     pub = rospy.Subscriber("/robot_news", String, callback_receive_data)
-    pub = rospy.Subscriber("/robot_news2", String, callback_receive_data2)
 
-    rospy.spin()
-
-    rospy.loginfo("Robot Subscriber finished")
+    while True:
+        rospy.loginfo("=== Printing ===")
+        for thread in threading.enumerate():
+            rospy.loginfo(thread.name)
